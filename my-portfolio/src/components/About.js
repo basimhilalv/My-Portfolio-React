5a287b96bba93f78b715c371769d5311dfd6ef0d
Basim Hilal V <basimhilalofficial@gmail.com>
2024-05-30 22:33:57 +0530

    image added

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -29,7 +29,7 @@ export default function About(){
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src="./IMG_0003.png"
           />
         </div>
       </div>
