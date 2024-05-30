581d4697ff35bbc9ffc173854ec5303d5bd52d5d
Basim Hilal V <basimhilalofficial@gmail.com>
2024-05-30 21:37:07 +0530

    contents update

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -5,14 +5,12 @@ export default function About(){
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Reed.
+            Hi, I'm Basim.
             <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            web apps.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            Web techs are my friends, and I just love to play with them !!. I am a Btech graduate from Kerala, India a passionate learner and tech enthusiast.
           </p>
           <div className="flex justify-center">
             <a
